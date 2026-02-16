adbf816e5fb8e5c9bf8ba4b256954447ddb55c24
Shrey Vats <techtechnicalshrey@gmail.com>
2026-02-16 19:49:58 +0530

    added the global routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,14 +1,72 @@
-
-use axum::{Router, middleware::from_fn_with_state};
+use axum::{
+    Router,
+    middleware::from_fn_with_state,
+    routing::{delete, get, post, put},
+};
 
 use crate::{
- middleware::auth::auth_middleware, modules::{progress::routes::progress_routes, todo::routes::todo_routes, user::routes::auth_router}, state::AppState
+    middleware::auth::auth_middleware,
+    modules::{
+        progress::handler::{
+            create_daily_progress_handler, create_daily_progress_todo_handler,
+            fetch_all_daily_progress_todos, fetch_daily_progress_todo_by_id,
+            toggle_daily_progress_todo_handler,
+        },
+        rooms::handler::{create_room_handler, get_all_rooms_handler, get_room_handler},
+        todo::handler::{
+            create_category_handler, create_tag_handler, create_todo_handler,
+            delete_category_handler, delete_tag_handler, delete_todo_handler,
+            fetch_all_categories_handler, fetch_all_tags_handler, get_todo_handler,
+            update_todo_handler,
+        },
+        user::handler::{create_user, delete_user_handler, get_user_handler, login_user},
+    },
+    state::AppState,
 };
 
 pub fn create_app(state: AppState) -> Router {
     Router::new()
-    .nest("/api", progress_routes()).route_layer(from_fn_with_state(state.clone(), auth_middleware))
-    .nest("/api", todo_routes()).route_layer(from_fn_with_state(state.clone(), auth_middleware))
-    .nest("/api", auth_router())
-    .with_state(state)
+        .nest("/api", protected_routes())
+        .route_layer(from_fn_with_state(state.clone(), auth_middleware))
+        .nest("/api", routes())
+        .with_state(state)
+}
+
+pub fn protected_routes() -> Router<AppState> {
+    Router::new()
+        .route("/todo/add", post(create_todo_handler))
+        .route("/todo/update/{id}", put(update_todo_handler))
+        .route("/todo/remove/{id}", delete(delete_todo_handler))
+        .route("/todo/get/{id}", get(get_todo_handler))
+        .route("/user/delete", delete(delete_user_handler))
+        .route("/user/me", get(get_user_handler))
+        .route("/tag/add", post(create_tag_handler))
+        .route("/tag/{slug}", get(delete_tag_handler))
+        .route("/tag/all", get(fetch_all_tags_handler))
+        .route("/category/add", post(create_category_handler))
+        .route("/category/{slug}", delete(delete_category_handler))
+        .route("/progress", post(create_daily_progress_handler))
+        .route("/progress/todo", post(create_daily_progress_todo_handler))
+        .route(
+            "/progress/todo/{progress_todo_id}",
+            get(fetch_daily_progress_todo_by_id),
+        )
+        .route(
+            "/progress/todo/{progress_todo_id}",
+            put(toggle_daily_progress_todo_handler),
+        )
+        .route(
+            "/progress/todos/{daily_progress_id}",
+            get(fetch_all_daily_progress_todos),
+        )
+        .route("/room", post(create_room_handler))
+        .route("/room/info/{room_id}", get(get_room_handler))
+        .route("/rooms", get(get_all_rooms_handler))
+    // .route("/room/{room_id}", get(ws_handler))
+}
+
+pub fn routes() -> Router<AppState> {
+    Router::new()
+        .route("/user/create", post(create_user))
+        .route("/user/login", post(login_user))
 }
